af75d02c353471ad542f58eb275c636ef4cda2bc
Yanson <wlarson30@gmail.com>
2020-01-30 23:18:35 -0500

    Updated the DegreeExample

--- examples/DegreeExample.cpp
+++ examples/DegreeExample.cpp
@@ -7,5 +7,12 @@ int main()
 
 	Math::Degree deg2(4);
 	std::cout << deg2 * 6 << std::endl;
+
+	std::cout << static_cast<float>(deg2) << std::endl;
+
+	Math::Degree deg3;
+	deg3 = Math::Degree(20);
+	std::cout << deg3 << std::endl;
+
 	return 0;
 }
