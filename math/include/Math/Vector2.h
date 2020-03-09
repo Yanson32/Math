a1c6323106bb0656adbc5c3dfd7a070773fa76df
Yanson <wlarson30@gmail.com>
2020-03-09 14:17:35 -0400

    Made Vector2 a template class

--- math/include/Math/Vector2.h
+++ math/include/Math/Vector2.h
@@ -9,6 +9,7 @@
 
 namespace Math
 {
+    template <class T>
 	class MATH_EXPORT Vector2
 	{
 	public:
@@ -23,14 +24,14 @@ namespace Math
 		*	@param	newX movement on the x axis.
 		*	@param	newY movement on the y axis.
 		****************************************************************/
-		Vector2(const float newX, const float newY);
+		Vector2(const T newX, const T newY);
 
 
 		/************************************************************//**
 		*	@brief	Get the vectors magnitude.
 		*	@return	The magnitude of the vector
 		****************************************************************/
-		float magnitude() const;
+		T magnitude() const;
 
 
 		/************************************************************//**
@@ -46,7 +47,7 @@ namespace Math
 		*	@return	A vector that has been scaled in comparison to
 		*		    this vector object.
 		****************************************************************/
-		Vector2 operator * (const float scalar);
+		Vector2<T> operator * (const T scalar);
 
 
 		/************************************************************//**
@@ -55,7 +56,7 @@ namespace Math
 		*	@return	A vector representing the difference between this
 		*			vector and the input vector (param)
 		****************************************************************/
-		Vector2 operator - (const Vector2 param);
+		Vector2<T> operator - (const Vector2<T> param);
 
 
 		/************************************************************//**
@@ -64,14 +65,14 @@ namespace Math
 		*	@return	A vector representing the sum of this
 		*			vector and the input vector (param)
 		****************************************************************/
-		Vector2 operator + (const Vector2 param);
+		Vector2<T> operator + (const Vector2<T> param);
 
 
 		/************************************************************//**
 		*	@brief	Set the magnitude of the vector
 		*	@param	magnitude the new magnitude of the vector.
 		****************************************************************/
-		void setMagnitude(const float magnitude);
+		void setMagnitude(const T magnitude);
 
 
 		/************************************************************//**
@@ -80,18 +81,141 @@ namespace Math
 		*			dot product against.
 		*	@return	The dot product of this vector and the input vector.
 		****************************************************************/
-		float dot(const Vector2 param);
+		float dot(const Vector2<T> param);
 
 
 		/************************************************************//**
 		*	@brief	Destructor
 		****************************************************************/
 		~Vector2();
-
-
-		float x = 0;
-		float y = 0;
+		T x = 0;
+		T y = 0;
 	};
+
+
+	/************************************************************//**
+	*	@brief	Default constructor
+	****************************************************************/
+	cVector<T>::cVector()
+	{
+
+	}
+
+
+	/************************************************************//**
+	*	@brief	Constructor with two arguments.
+	*	@param	newX movement on the x axis.
+	*	@param	newY movement on the y axis.
+	****************************************************************/
+	cVector<T>::cVector(const T newX, const T newY) :
+		x(newX),
+		y(newY)
+	{
+
+	}
+
+
+	/************************************************************//**
+	*	@brief	Get the vectors magnitude.
+	*	@return	The magnitude of the vector
+	****************************************************************/
+	T cVector<T>::magnitude() const
+	{
+		//Use pythagorean theorem to calculate magnitude
+		return std::sqrt(x * x + y * y);
+	}
+
+
+	/************************************************************//**
+	*	@brief	Set the vectors magnitude to one, with the same
+	*			direction.
+	****************************************************************/
+	void cVector<T>::normalize()
+	{
+		T mag = magnitude();
+		if (!mag)
+			throw std::runtime_error("cannot divide by zero [cVector::normalize]");
+
+		x = x / mag;
+		y = y / mag;
+	}
+
+
+	/************************************************************//**
+	*	@brief	Scale the vector according to the input param.
+	*	@param	scalar the amout of scaling to occur.
+	*	@return	A vector that has been scaled in comparison to
+	*		    this vector object.
+	****************************************************************/
+	cVector<T> cVector<T>::operator * (const T scalar)
+	{
+		T mag = (magnitude() * scalar);
+		cVector vec;
+		vec.setMagnitude(mag);
+		return vec;
+	}
+
+
+	/************************************************************//**
+	*	@brief	Get the difference between two vectors
+	*	@param	param a vector to subtract from this vector object.
+	*	@return	A vector representing the difference between this
+	*			vector and the input vector (param)
+	****************************************************************/
+	cVector<T> cVector<T>::operator - (const cVector<T> param)
+	{
+		T xAxis = x - param.x;
+		T yAxis = y - param.y;
+		return cVector<T>(xAxis, yAxis);
+	}
+
+
+	/************************************************************//**
+	*	@brief	Get the sum of two vectors
+	*	@param	param a vector to be added to this vector object.
+	*	@return	A vector representing the sum of this
+	*			vector and the input vector (param)
+	****************************************************************/
+	cVector<T> cVector<T>::operator + (const cVector<T> param)
+	{
+		T xAxis = x + param.x;
+		T yAxis = y + param.y;
+		return cVector<T>(xAxis, yAxis);
+	}
+
+
+	/************************************************************//**
+	*	@brief	Set the magnitude of the vector
+	*	@param	magnitude the new magnitude of the vector.
+	****************************************************************/
+	void cVector<T>::setMagnitude(const T magnitude)
+	{
+		normalize();
+		cVector<T> vec = *this * magnitude;
+		x = vec.x;
+		y = vec.y;
+	}
+
+
+	/************************************************************//**
+	*	@brief	Calculate the dot product of two vectors.
+	*	@param	param a vector that will be used to calculate the
+	*			dot product against.
+	*	@return	The dot product of this vector and the input vector.
+	****************************************************************/
+	T cVector<T>::dot(const cVector<T> param)
+	{
+		return x  * param.x + y * param.y;
+	}
+
+
+	/************************************************************//**
+	*	@brief	Destructor
+	****************************************************************/
+	cVector::~cVector()
+	{
+
+	}
 }
 
 #endif
