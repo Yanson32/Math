701acbce3117a86192d4114759e66fa4e6807032
Yanson <wlarson30@gmail.com>
2020-03-13 02:44:39 -0400

    Refactor: removed unneeded files

--- math/src/Math/Functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "Math/Functions.h"
-#include "Math/Constants.h"
-#include "Math/Radian.h"
-#include "Math/Degree.h"
-#include <cmath>
-namespace Math
-{
-
-}
--- math/src/Math/Vector.cpp
+++
@@ -1,6 +0,0 @@
-#include "Math/Vector.h"
-#include "Math/Functions.h"
-namespace Math
-{
-
-}
