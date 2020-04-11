38eb4f636f1e5c4f9039691fc86fd9eea4d09812
Yanson32 <wlarson30@gmail.com>
2020-04-11 01:26:31 -0400

    Added example for Radian class

---
+++ examples/RadianExample.cpp
@@ -0,0 +1,14 @@
+#include "Math/Radian.h"
+#include "Math/Degree.h"
+#include <iomanip>
+
+int main()
+{
+	Math::Degree<float> rad2(3.25);
+    
+    //Convert Math::Radian to Math::Degree
+    std::cout << rad2 << " " << Math::Degree<float>(rad2) << std::endl;
+
+
+	return 0;
+}
