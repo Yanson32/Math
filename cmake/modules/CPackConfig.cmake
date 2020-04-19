4750fc615933929ba75531ec66140dd4bdacf700
Yanson32 <wlarson30@gmail.com>
2020-04-18 23:25:54 -0400

    Added package config file

---
+++ cmake/modules/CPackConfig.cmake
@@ -0,0 +1,7 @@
+set(CPACK_RESOURCE_FILE_README "${Math_SOURCE_DIR}/README.MD")
+set(CPACK_PACKAGE_VENDOR "Yanson Tech")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "${Math_SOURCE_DIR}/cmake/Description.txt")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "This package provides tools for manipulating media files")
+set(CPACK_PACKAGE_ICON "")
+set(CPACK_PACKAGE_EXECUTABLES "Math")
+
