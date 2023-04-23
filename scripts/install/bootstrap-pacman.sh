37ceeb8658e65a598a4a0fc8001ef430acffa486
Yanson32 <wlarson30@gmail.com>
2023-04-23 17:30:30 -0400

    Added the scripts directory

---
+++ scripts/install/bootstrap-pacman.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Update the system
+sudo pacman -Syu --noconfirm
+
+#install gcc
+sudo pacman -S gcc --noconfirm
+
+#Install cmake
+sudo pacman -S cmake --noconfirm
+
+#Update the linker
+sudo ldconfig
